fe34c0d6881180e10410198067305367fbffd867
felipeteste11 <felipe.157045@gmail.com>
2022-10-13 08:42:18 -0300

    a

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,10 +15,10 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
- /*  viteFinal: (config, {configType}) => {
+  viteFinal: (config, {configType}) => {
     if (configType === 'PRODUCTION') {
       config.base = '/lab-design-system/'
     }
     return config
-  } */
+  }
 }
